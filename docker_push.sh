df005216870465bdb2a0c4cd2e0b9f0647541e37
Alan Lam <git@syssolhk.com>
2019-04-05 07:31:09 +0100

    fix wrong tag for local docker image built

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker ps -a
-docker tag dockcross/base $DOCKER_USERNAME/dockcross:base
+docker tag $DOCKER_USERNAME/base:latest $DOCKER_USERNAME/dockcross:base
 docker push $DOCKER_USERNAME/dockcross:base
