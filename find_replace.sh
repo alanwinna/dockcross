fd4a39a2b2a5b8b786a56c98f249673d045f4a8c
Alan Lam <git@syssolhk.com>
2019-04-06 07:00:38 +0100

    update location of base

--- find_replace.sh
+++ find_replace.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for i in `find . -name "Dockerfile.in"`; do
-    sed -i 's/^FROM dockcross\/base:latest/FROM pylam\/base:latest/g' $i
+    sed -i 's/^FROM dockcross\/base:latest/FROM pylam\/dockcross:base/g' $i
 done
 
 for i in `find . -name "Dockerfile"`; do
-    sed -i 's/^FROM dockcross\/base:latest/FROM pylam\/base:latest/g' $i
+    sed -i 's/^FROM dockcross\/base:latest/FROM pylam\/dockcross:base/g' $i
 done
